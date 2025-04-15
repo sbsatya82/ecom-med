5b871fba5772fe278d9995d66e70ef1b69f08506
sbsatya82 <satyabratamahanta82@gmail.com>
2025-04-16 00:12:49 +0530

    bug fix

--- server/index.js
+++ server/index.js
@@ -59,7 +59,7 @@ app.use('/api/order', orderRouter);
 if(process.env.NODE_ENV === 'production'){
     app.use(express.static(path.join(__dirname, '../client/dist')));
   
-    app.get("*",(req, res) => {
+    app.get("/",(req, res) => {
       res.sendFile(path.join(__dirname, '../client/dist/index.html'));
     })
   }
